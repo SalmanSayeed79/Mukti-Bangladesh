c83b5e1be5490dc1bb2d49169870f74d756e4341
SalmanSayeed79 <salmansayeed5345@gmail.com>
2023-07-31 12:17:53 +0600

    Completed Backend

---
+++ mukti-backend/src/main/java/com/mukti/muktibackend/Interface/UserRepository.java
@@ -0,0 +1,18 @@
+package com.mukti.muktibackend.Interface;
+
+
+import com.mukti.muktibackend.Entity.Alumni;
+import com.mukti.muktibackend.Entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    @Query(value = "SELECT * FROM  mukti_user",nativeQuery = true)
+    List<User> getAllUsers();
+
+
+    @Query(value = "SELECT * FROM  mukti_user a  WHERE   a.email = ?1",nativeQuery = true)
+    User searchUserByemail(String email);
+}
